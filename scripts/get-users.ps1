7fc415bcf88f5e35bce3374af966858435a53fe9
Kristian Nygaard Jensen <KristianNygaard.Jensen@ufst.d>
2022-10-11 13:07:26 +0200

    Limit properties for Get-ADUser to email and displayname, for extra speed

--- scripts/get-users.ps1
+++ scripts/get-users.ps1
@@ -1,2 +1,2 @@
-Get-ADGroup -Filter {Name -eq "openshiftusers" -or Name -eq "openshiftuserstst"}  -Properties * | Get-ADGroupMember | Get-ADUser -Filter * -Properties EmailAddress,DisplayName| select EmailAddress, DisplayName | export-csv -Encoding utf8 brugereutf8
+Get-ADGroup -Filter {Name -eq "openshiftusers" -or Name -eq "openshiftuserstst"}  -Properties * | Get-ADGroupMember | Get-ADUser -Filter {ObjectCategory -eq "person"} -Properties EmailAddress, DisplayName |  select EmailAddress, DisplayName
 
