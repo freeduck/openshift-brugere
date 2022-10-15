f9de4bca063f321ee9a6b86ae8fe6c0c29b01e13
Kristian Nygaard Jensen <KristianNygaard.Jensen@ufst.dk>
2022-10-15 12:55:55 +0200

    Alle brugere

--- project-user-list.sh
+++ project-user-list.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
-while read p; do
-    echo "\"$p\" "
-done < <(oc get projects --no-headers=true | awk '{print $1}')
+set -euo pipefail
+scriptdir=$(cd $(dirname $0) && pwd)
+projectdir_prefix=project-info
+main () {
+    while read p; do
+	echo "\"$p\" "
+	projectdir=$scriptdir/$projectdir_prefix/$p
+	echo "$projectdir"
+	mkdir -p $projectdir
+	groups $p $projectdir
+    done < <(oc get projects --no-headers=true | awk '{print $1}' | sort)
+}
+
+groups () {
+    project=$1
+    shift
+    projectdir=$1
+    shift
+    grep $project adgroups.txt
+    echo "OC groups"
+    grep $project allgroups
+    while read g; do
+	groupdir=$projectdir/$g
+	mkdir -p $groupdir
+	grep $project allgroups| awk '{print substr($0,index($0, $2))}' > $groupdir/brugere
+    done < <(grep $project allgroups| awk '{print $1}')
+}
+
+main
