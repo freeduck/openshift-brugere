ae3c12081d48b9b5f740d60dc5fadea9fd28841f
Kristian Nygaard Jensen <KristianNygaard.Jensen@ufst.dk>
2022-10-17 23:48:21 +0200

    ` Look I'm so funcking special
    Nårh ja det er nok noget med stien, men stadig

---
+++ scripts/find-kc.ps1
@@ -0,0 +1,40 @@
+function begin {
+	param (
+		$inputFile
+	)
+	Get-Content $inputFile | ForEach-Object {
+		$Line = $_;
+		$users = $Line.split(",");
+		$departments = @{}
+		For($i=0; $i -lt $users.Length; $i++){
+			$user = $users[$i].Trim()
+			$user_department = get-kc $user
+			if (-Not $departments.ContainsKey($user_department.Department)){
+				$departments.Add($user_department.Department, @{
+					KC = $user_department.KC
+					Users = [System.Collections.Generic.List[string]]::new()
+				})
+				
+			}
+			$departments[$user_department.Department].users.Add($user)
+		}
+		Write-Output $departments
+		$departments.GetEnumerator() | select @{N='Department';E={$_.Key}}, `
+			@{N='KC';E={$_.Value.KC.displayName + ',' + $_.Value.KC.mail}}, `
+			@{N='Brugere';E={$_.Value.users -join ","}}|Export-CSV -Encoding utf8 hesthjort 
+	}
+}
+
+function get-kc {
+	param (
+		$userid
+	)
+	$department = Get-ADUser -Filter "cn -eq ""$userid""" -ResultSetSize 1 -Properties Department | select -ExpandProperty Department
+	$kc = Get-ADUser -Filter {Department -eq $department -and title -eq "KC" } -ResultSetSize 1 -Properties displayName,mail | select displayName,mail
+	@{
+		Department = $department
+		KC = $kc
+	}
+}
+
+begin $args[0];
